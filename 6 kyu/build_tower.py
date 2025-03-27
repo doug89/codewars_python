bbac0231eb147d7a13525d32827ecc66e5fbd828
Douglas Kelly <douglas.perth@gmail.com>
2025-03-27 19:56:46 +0800

    Added solutions with center() and a one liner to build_tower.py

--- 6 kyu/build_tower.py
+++ 6 kyu/build_tower.py
@@ -2,7 +2,6 @@
 Build Tower
 6 kyu
 https://www.codewars.com/kata/576757b1df89ecf5bd00073b/python
-
 Description:
 Build Tower
 
@@ -11,14 +10,23 @@ Build a pyramid-shaped tower, as an array/list of strings, given a positive inte
 For example, a tower with 3 floors looks like this:
 """
 
-def tower_builder(n_floors):
-    building = []
-    for i in range(1, n_floors+1):
-        whitespace = " " * (n_floors - i)
-        floor = "*" * (1 + 2 * (i-1))
-        building.append(whitespace + floor + whitespace)
-    return building
+# def tower_builder(n_floors):
+#     building = []
+#     for i in range(1, n_floors+1):
+#         whitespace = " " * (n_floors - i)
+#         floor = "*" * (1 + 2 * (i-1))
+#         building.append(whitespace + floor + whitespace)
+#     return building
 
+# def tower_builder(n_floors):
+#     building = []
+#     for i in range(0, n_floors):
+#         floor = "*" * (1 + (2 * (i)))
+#         building.append(floor.center(1 + (2 * (n_floors - 1))))
+#     return building
+
+def tower_builder(n):
+    return [("*" * (1 + 2 * i)).center(1 + 2 * (n - 1)) for i in range(n)]
 
 # def tower_builder(n_floors):
 #     building = []
